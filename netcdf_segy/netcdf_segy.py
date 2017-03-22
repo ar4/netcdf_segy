bae0bc5139e48b86068243b13b20eecb84b79730
Alan Richardson <arichar@tcd.ie>
2017-03-22 12:12:39 +0000

    expanding help message

--- netcdf_segy/netcdf_segy.py
+++ netcdf_segy/netcdf_segy.py
@@ -6,20 +6,33 @@ from netCDF4 import Dataset
 @click.command()
 @click.argument('segy', type=click.Path(exists=True, dir_okay=False))
 @click.argument('netcdf', type=click.Path())
-@click.option('-d0', type=str, help="name of first (trace samples) dimension")
-@click.option('-d', type=(str, int), multiple=True, help="dimension name and length")
-def segy2netcdf(segy, netcdf, d0, d):
+@click.option('--samples_dim_name', '-sdn', type=str,
+        help="Name of trace samples dimension (usually Time or Depth)")
+@click.option('-d', type=(str, int), multiple=True,
+        help='Name and length (separated by a space) ' +
+        'of other dimensions, in fastest to slowest order. If the name ' +
+        'matches the name of a trace header field (using segyio.TraceField ' +
+        'names), then the header values will be used for the dimension ' +
+        'coordinates, otherwise the dimension coordinates will start at ' +
+        '0 and increment by 1. ' +
+        'E.g. -d GroupNumber 5 -d ShotPoint 10, indicates that the data ' +
+        'consists of 10 shot gathers, each with 5 receiver traces. As ' +
+        'ShotPoint is a trace header name, the trace header values will be ' +
+        'used as coordinates for that dimension. GroupNumber is not a ' +
+        'header name, so the coordinates of that dimension will be 0 to 4.')
+def segy2netcdf(segy, netcdf, samples_dim_name, d):
 
-    # set default name for trace samples dimension to 'samples'
-    if not d0:
-        d0 = 'SampleNumber'
+    # set default name for trace samples dimension
+    if not samples_dim_name:
+        samples_dim_name = 'SampleNumber'
     print(d)
 
     with segio.open(segy_path) as segy:
         ns = len(segy.samples)
+        ds = segy.dt
         ntraces = segy.tracecount
 
-        dim_names, dim_lens = _make_dim_name_len(d0, ns, d)
+        dim_names, dim_lens = _make_dim_name_len(samples_dim_name, ns, d)
         dims_ntraces = _count_traces_in_user_dims(d)
 
         _check_user_dims(dims_ntraces, ntraces)
@@ -41,14 +54,13 @@ def segy2netcdf(segy, netcdf, d0, d):
 def netcdf2segy(netcdf, segy):
     click.echo('hi n2s %s %s' % (netcdf, segy))
 
-def _make_dim_name_len(d0, ns, d):
-    dim_names = [d0]
+def _make_dim_name_len(samples_dim_name, ns, d):
+    dim_names = [samples_dim_name]
     dim_lens = [ns]
     for dim in d:
         dim_names.append(dim[0])
         dim_lens.append(dim[1])
     return dim_names, dim_lens
-
 
 def _count_traces_in_user_dims(d):
     dims_ntraces = 1
@@ -58,10 +70,10 @@ def _count_traces_in_user_dims(d):
 
 def _check_user_dims(dims_ntraces, ntraces):
     if dims_ntraces > ntraces:
-        raise ValueError('supplied dimensions imply %d traces, but only %d in file' %
-                (dims_ntraces, ntraces))
+        raise ValueError('supplied dimensions imply %d traces, ' +
+        'but only %d in file' % (dims_ntraces, ntraces))
     if ntraces % dims_ntraces != 0:
-        raise ValueError('''supplied dimensions imply %d traces, 
+        raise ValueError('supplied dimensions imply %d traces, ' +
         but this does not divide into the %d traces in the file''' %
         (dims_ntraces, ntraces))
 
@@ -83,6 +95,7 @@ def _copy_data(segy, variable, dims):
     dim_names = _get_dim_names(dims)
     header_fields = _get_header_fields(dim_names)
     for trace in segy.traces:
+        pass
 
 def _get_dim_names(dims):
     dim_names = []
