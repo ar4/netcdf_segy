8b705dcfd75bc3b5f6200d3a7afa9c0761cd343c
Alan Richardson <arichar@tcd.ie>
2017-03-23 19:44:38 +0000

    just started netcdf2segy

--- netcdf_segy/netcdf2segy.py
+++ netcdf_segy/netcdf2segy.py
@@ -5,11 +5,30 @@ import numpy as np
 from netCDF4 import Dataset
 
 @click.command()
-@click.argument('netcdf', type=click.Path(exists=True, dir_okay=False))
-@click.argument('segy', type=click.Path())
-def cli(netcdf, segy):
-    netcdf2segy(netcdf, segy)
+@click.argument('netcdf_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('segy_path', type=click.Path())
+def cli(netcdf_path, segy_path):
+    netcdf2segy(netcdf_path, segy_path)
 
-def netcdf2segy(netcdf, segy):
-    click.echo('hi n2s %s %s' % (netcdf, segy))
+def netcdf2segy(netcdf_path, segy_path):
+    rootgrp = Dataset(netcdf_path, "r", format="NETCDF4")
+    spec = _create_segy_spec()
+    with segyio.create(segy_path, spec) as segy:
+        pass
 
+    rootgrp.close()
+
+def _create_segy_spec(rootgrp, samples_dim_name):
+    spec = segyio.spec()
+    spec.iline = 0
+    spec.ilines = None
+    spec.xline = 0
+    spec.xlines = None
+    spec.offsets = [1]
+    spec.samples = rootgrp[samples_dim_name]
+    spec.tracecount = rootgrp[] #TODO
+    spec.ext_headers = int('ext_headers' in rootgrp)
+    spec.format = None
+    spec.sorting = None
+    spec.t0 = 0.0
+    return spec
