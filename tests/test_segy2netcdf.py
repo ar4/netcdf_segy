6d1825e9ec0d6a73e10b0038da8b078d38c8d614
Alan Richardson <arichar@tcd.ie>
2017-03-23 18:06:44 +0000

    removed parenthses from assert statements

--- tests/test_segy2netcdf.py
+++ tests/test_segy2netcdf.py
@@ -58,22 +58,22 @@ def segy1():
 class Test_make_dim_name_len:
     def test_all_args(self, d_all_headers):
         dim_names, dim_lens = segy2netcdf._make_dim_name_len('Time', 5, d_all_headers)
-        assert(dim_names == ['ShotPoint', 'GroupX', 'Time'])
-        assert(dim_lens == [3, 10, 5])
+        assert dim_names == ['ShotPoint', 'GroupX', 'Time']
+        assert dim_lens == [3, 10, 5]
 
     def test_no_d(self):
         dim_names, dim_lens = segy2netcdf._make_dim_name_len('Time', 5, ())
-        assert(dim_names == ['Time'])
-        assert(dim_lens == [5])
+        assert dim_names == ['Time']
+        assert dim_lens == [5]
 
 class Test_count_traces_in_user_dims:
     def test_d(self, d_all_headers):
         dims_ntraces = segy2netcdf._count_traces_in_user_dims(d_all_headers)
-        assert(dims_ntraces == 3 * 10)
+        assert dims_ntraces == 3 * 10
 
     def test_no_d(self):
         dims_ntraces = segy2netcdf._count_traces_in_user_dims(())
-        assert(dims_ntraces == 1)
+        assert dims_ntraces == 1
 
 class Test_check_user_dims:
     def test_full_d(self):
@@ -109,44 +109,44 @@ class Test_fill_missing_dims:
         dim_names = ['ShotPoint', 'GroupX', 'Time']
         dim_lens = [3, 10, 20]
         segy2netcdf._fill_missing_dims(30, 30, dim_names, dim_lens)
-        assert(dim_names == ['ShotPoint', 'GroupX', 'Time'])
-        assert(dim_lens == [3, 10, 20])
+        assert dim_names == ['ShotPoint', 'GroupX', 'Time']
+        assert dim_lens == [3, 10, 20]
 
     def test_partial_d(self):
         dim_names = ['ShotPoint', 'GroupX', 'Time']
         dim_lens = [3, 5, 20]
         segy2netcdf._fill_missing_dims(15, 30, dim_names, dim_lens)
-        assert(dim_names == ['Traces', 'ShotPoint', 'GroupX', 'Time'])
-        assert(dim_lens == [2, 3, 5, 20])
+        assert dim_names == ['Traces', 'ShotPoint', 'GroupX', 'Time']
+        assert dim_lens == [2, 3, 5, 20]
 
     def test_no_d(self):
         dim_names = ['Time']
         dim_lens = [20]
         segy2netcdf._fill_missing_dims(1, 30, dim_names, dim_lens)
-        assert(dim_names == ['Traces', 'Time'])
-        assert(dim_lens == [30, 20])
+        assert dim_names == ['Traces', 'Time']
+        assert dim_lens == [30, 20]
 
     def test_zero_d_zero_ntraces(self):
         dim_names = ['Time']
         dim_lens = [0]
         segy2netcdf._fill_missing_dims(0, 0, dim_names, dim_lens)
-        assert(dim_names == ['Time'])
-        assert(dim_lens == [0])
+        assert dim_names == ['Time']
+        assert dim_lens == [0]
 
 class Test_create_dimensions:
     def test_multiple_dims(self, rootgrp_empty, dim_names1, dim_lens1):
         segy2netcdf._create_dimensions(dim_names1, dim_lens1, rootgrp_empty)
         for name, dim in rootgrp_empty.dimensions.items():
-            assert(name in dim_names1)
+            assert name in dim_names1
             idx = dim_names1.index(name)
-            assert(dim.size == dim_lens1[idx])
+            assert dim.size == dim_lens1[idx]
 
     def test_zero_len_dim(self, rootgrp_empty, dim_names1, dim_lens1):
         segy2netcdf._create_dimensions(dim_names1, dim_lens1, rootgrp_empty)
         for name, dim in rootgrp_empty.dimensions.items():
-            assert(name in dim_names1)
+            assert name in dim_names1
             idx = dim_names1.index(name)
-            assert(dim.size == dim_lens1[idx])
+            assert dim.size == dim_lens1[idx]
 
 class Test_create_variables:
     def test_1(self, rootgrp_dims, dim_names1, dim_lens1):
@@ -155,18 +155,18 @@ class Test_create_variables:
                 if not callable(getattr(segyio.TraceField, attr))
                 and not attr.startswith("__")]
         for name, var in rootgrp_dims.variables.items():
-            assert(name in (['Samples'] + dim_names1 + fields))
+            assert name in (['Samples'] + dim_names1 + fields)
             if name == 'Samples':
-                assert(var.dimensions == tuple(dim_names1))
-                assert(var.dtype == np.float32)
-                assert(var.shape == tuple(dim_lens1))
+                assert var.dimensions == tuple(dim_names1)
+                assert var.dtype == np.float32
+                assert var.shape == tuple(dim_lens1)
             elif name in dim_names1:
                 idx = dim_names1.index(name)
-                assert(var.dimensions == (name,))
-                assert(var.shape == (dim_lens1[idx],))
+                assert var.dimensions == (name,)
+                assert var.shape == (dim_lens1[idx],)
             else:
-                assert(var.dimensions == tuple(dim_names1[:-1]))
-                assert(var.shape == tuple(dim_lens1[:-1]))
+                assert var.dimensions == tuple(dim_names1[:-1])
+                assert var.shape == tuple(dim_lens1[:-1])
 
 def check_data1(dataset):
     for name, v in dataset.items():
@@ -193,42 +193,42 @@ def check_data1(dataset):
         else:
             it = np.nditer(v[:], ['c_index'], ['readonly'])
             while not it.finished:
-                assert(it[0] == 0)
+                assert it[0] == 0
                 it.iternext()
 
 def check_samples1(data):
-    assert(data.size == 3 * 10 * 20)
+    assert data.size == 3 * 10 * 20
     it = np.nditer(data, ['c_index'], ['readonly'])
     while not it.finished:
         assert np.isclose(it[0], it.index + 123.456, atol=1e-4)
         it.iternext()
 
 def check_time1(data):
-    assert(len(data) == 20)
+    assert len(data) == 20
     for i, v in enumerate(data):
         assert np.isclose(v, i * 1234.0, atol=1e-1)
 
 def check_fieldrecord1(data):
-    assert(len(data) == 3)
+    assert len(data) == 3
     for i, v in enumerate(data):
         assert v == 777 + i
 
 def check_groupx1(data):
-    assert(data.size == 3 * 10)
+    assert data.size == 3 * 10
     it = np.nditer(data, ['c_index'], ['readonly'])
     while not it.finished:
         assert it[0] == 456789+1000*(it.index % 10)
         it.iternext()
 
 def check_allconst(data, value):
-    assert(data.size == 3 * 10)
+    assert data.size == 3 * 10
     it = np.nditer(data, [], ['readonly'])
     while not it.finished:
         assert it[0] == value
         it.iternext()
 
 def check_linear(data):
-    assert(data.size == 3 * 10)
+    assert data.size == 3 * 10
     it = np.nditer(data, ['c_index'], ['readonly'])
     while not it.finished:
         assert it[0] == 1 + it.index
@@ -238,4 +238,3 @@ class Test_copy_data:
     def test_copy1(self, segy1, rootgrp_vars, dim_names1, dim_lens1):
         segy2netcdf._copy_data(segy1, rootgrp_vars.variables.values(), dim_names1, dim_lens1, False)
         check_data1(rootgrp_vars.variables)
-
