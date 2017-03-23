edebbeaca508aae0a882dec5ecb65a86a8f2140e
Alan Richardson <arichar@tcd.ie>
2017-03-23 18:45:59 +0000

    added note to script for making test segy that needs Seismic Unix

--- tests/make_testsegy1.sh
+++ tests/make_testsegy1.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Make simple SEG-Y file for testing
+# Requires Seismic Unix
 
 python make_testbin1.py
 suaddhead < testbin1.bin ns=20 | sushw key=dt,fldr,gx,scalco a=1234,777,456789,-1000 b=0,0,1000,0 c=0,1,0,0 j=ULONG_MAX,10,10,ULONG_MAX > testsegy1.su
